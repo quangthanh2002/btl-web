3aacc59a2a457ef99b7d3691ec01a29cab4bffb7
2051063957 <2051063957@e.tlu.edu.vn>
2022-04-14 17:22:02 +0700

    Update script.js

--- web/script.js
+++ web/script.js
@@ -53,3 +53,23 @@ function imgAuto () {//tự động chạy
     // console.log(index)
 }
 setInterval(imgAuto,5000)
+//---------slider-product------------
+const rightbtntwo = document.querySelector('.fa-chevron-right-two')
+const leftbtntwo = document.querySelector('.fa-chevron-left-two')
+const imgNubertwo = document.querySelectorAll('.slider-product-one-content-items')
+// console.log(rightbtntwo)
+// console.log(leftbtntwo)
+rightbtntwo.addEventListener("click", function(){
+    index = index + 1
+    if(index>imgNubertwo.length-1){
+        index=0
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})
+leftbtntwo.addEventListener("click", function(){
+    index = index - 1
+    if(index<=0){
+        index=imgNubertwo.length-1
+    }
+    document.querySelector(".slider-product-one-content-items-content").style.right = index *100+"%"
+})

0f550a51e76dad6d6eba5eedd9fd4636b5aa626d
2051063957 <2051063957@e.tlu.edu.vn>
2022-04-14 16:38:01 +0700

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,5 +1,6 @@
 const adressbtn = document.querySelector('#adress-form')
 const adressclose = document.querySelector('adress-close')
+
 // console.log (rightbtn)
 adressbtn.addEventListener("click", function(){
     document.querySelector('adress-form').style.display = "flex"
@@ -9,6 +10,46 @@ adressbtn.addEventListener("click", function(){
 // })
 // slide------------
 const rightbtn = document.querySelector('.fa-chevron-right')
+const lefttbtn = document.querySelector('.fa-chevron-left')
+const imgNuber = document.querySelectorAll('.slider-content-left-top img')
+let index = 0 //let là biến không đổi
+
 rightbtn.addEventListener("click", function(){
-    document.querySelector(".slider-content-left-top").style.right = "100%"
+    index = index + 1
+    if(index>imgNuber.length-1){
+        index=0
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
 })
+lefttbtn.addEventListener("click", function(){
+    index = index - 1
+    if(index<=0){
+        index=imgNuber.length-1
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
+//slider 1------
+const imgNuberLi = document.querySelectorAll('.slider-content-left-bottom li')
+imgNuberLi.forEach(function(image,index){// điều chỉnh thẻ li dưới ảnh
+    image.addEventListener("click",function(){
+        removeactive ()
+        document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+        image.classList.add("active")//thêm boder
+    })
+})
+function removeactive (){
+    let imgactive = document.querySelector('.active')
+    imgactive.classList.remove("active")
+}
+//slider2
+function imgAuto () {//tự động chạy
+    index = index + 1
+    if(index>imgNuber.length-1){
+        index=0
+    }
+    removeactive ()
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+    imgNuberLi[index].classList.add("active")
+    // console.log(index)
+}
+setInterval(imgAuto,5000)
